3d33085b46030437110d7c5c5f8f7cad76014261
Szigeti Balázs <balazs.mindenes@gmail.com>
2025-02-15 02:11:09 +0100

    ♻️ Seed folyamat refaktorálása, kommentelés

--- src/db/seeds/auth/groups/groups.ts
+++ src/db/seeds/auth/groups/groups.ts
@@ -4,6 +4,11 @@ import { groups as groupSeedConfig } from '@/db/seedConfig'
 
 const initData: GroupSchema[] = Object.values(groupSeedConfig)
 
+/**
+ * Inicializálja a csoportok táblát a seedConfig-ban megadott adatokkal.
+ *
+ * @param db Az adatbázis példány.
+ */
 export async function seed(db: DB) {
 	await db.insert(groups).values(initData)
 }

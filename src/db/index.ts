beda7355d9f9d27bec397308c7e585c8603d093e
Szigeti Balázs <balazs.mindenes@gmail.com>
2025-02-28 14:47:56 +0100

    Savepoint

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,17 +1,22 @@
-import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
+import { drizzle } from 'drizzle-orm/node-postgres'
+import { Pool } from 'pg'
 
 import { env } from '@/lib/env'
 
 import * as schema from './schemas'
 
-const client = postgres(env.DATABASE_URL, {
+/*const client = postgres(env.DATABASE_URL, {
+	max: env.DB_MIGRATING ? 1 : undefined,
+})*/
+const pool = new Pool({
+	connectionString: env.DATABASE_URL,
 	max: env.DB_MIGRATING ? 1 : undefined,
 })
-const db = drizzle(client, {
+
+const db = drizzle(pool, {
 	schema,
 })
 
 export default db
-export { client }
+export { pool as client }
 export type DB = typeof db
